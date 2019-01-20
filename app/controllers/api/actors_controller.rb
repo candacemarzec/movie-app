d973ebe12e2e1acd9779635a2a658b7cacb54d1f
candace <candace.marzec@gmail.com>
2019-01-19 20:17:27 -0800

    adds new routes

--- app/controllers/api/actors_controller.rb
+++ app/controllers/api/actors_controller.rb
@@ -3,8 +3,14 @@ class Api::ActorsController < ApplicationController
   def single_actor
     @actor = Actor.find_by(id: 1)
     render 'single_actor.json.jbuilder'
-  end  
+  end 
+
+  def single_actor_params
+    actor_id = params[:id]
+    @actor = Actor.find_by(id: actor_id)
+    render 'single_actor.json.jbuilder' 
+
+  end
+
 
 end
-
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
-  # STEP 1: A ROUTE triggers a controller action
-  # verb "/urls" => "namespace/controllers#action"
+  
   namespace :api do
+
     get "/single_actor_url" => "actors#single_actor"
+
     get "/all_movies_url" => "movies#all_movies_method"
+
     get "/last_movie_url" => "movies#last_movie_method"
+
     get "/descending_movie_url" => "movies#descending_movie_method"
+
+    get "single_query" => "actors#single_actor_params"
+
+    get "single_segment/:id" =>
+    "actors#single_actor_params"
+
+    post "single_body" => "actors#single_actor_params"
   end  
 end
