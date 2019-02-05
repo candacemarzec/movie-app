3bbcea09f25a8a37b04b809508c41a15eabadfca
candace <candace.marzec@gmail.com>
2019-02-04 19:02:47 -0800

    adds create action for moviegenre

---
+++ app/controllers/api/movie_genres_controller.rb
@@ -0,0 +1,12 @@
+class Api::MovieGenresController < ApplicationController
+
+  def create
+    @movie_genre = MovieGenre.new(
+      movie_id: params["movie_id"],
+      genre_id: params["genre_id"]
+      )
+    @movie_genre.save
+    render json: {message: "MovieGenre successfully created."}
+  end
+
+end
